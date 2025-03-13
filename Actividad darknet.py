3bc3601585eb19dada6f8ac2f5aab4f5ecdfcb21
Nikki1614 <163075510+Nikki1614@users.noreply.github.com>
2025-03-13 12:13:10 -0500

    Add files via upload

--- Actividad darknet.py
+++ Actividad darknet.py
@@ -14,24 +14,33 @@ df = pd.read_csv(url)
 #df1=df.dropna() #Elimina campos con celdas que esten vacias
 #print(len(df))
 #print(len(df1))
-df2=df.fillna("Vacio") #Llena los campos vacios
-df2["Timestamp"]= pd.to_datetime(df2["Timestamp"])
 
+df2=df.fillna(0) #Llena los campos vacios
+df2.isnull().sum()
+df2["Timestamp"]= pd.to_datetime(df2["Timestamp"])
 print(df2["Timestamp"])
+
 df2["Flow ID"] = df2["Flow ID"].astype(str)
 df2["Src IP"] = df2["Src IP"].astype(str)
 df["Dst IP"]=df2["Dst IP"].astype(str)
-df2["Timestamp"]= pd.to_datetime(df2["Timestamp"])
+
 
 df2["Label"]=df2["Label"].astype('category')
-df2["Label"]=df2["Label"].astype('category')
+df2["Label.1"]=df2["Label.1"].astype('category')
 
 #Tipos modificados despues de conversión
 print(df2.dtypes)
-#Verificar si esta convirtiendo los objetos en string
+print("\n\nVerificar si esta convirtiendo los objetos en string\n")
 
 print(df["Flow ID"].apply(type).value_counts())
 print(df["Dst IP"].apply(type).value_counts())
 print(df["Src IP"].apply(type).value_counts())
+
+
+print("\nResumen estadistico")
+print(df2.select_dtypes(include=['number']).describe())
+
+
+
 
 df2.to_csv("Darknetlimpio.csv", index=False)
